8465e7e37af96620c0eb83dfafbce2be5de27bd5
Augusto Ruiz Clemente <augusto@realhost.com.mx>
2022-04-08 11:42:39 -0500

    Update component navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,70 @@
 import React from "react";
 
-import NavbarItem from "./components/item";
+import NavbarItem from "./components/Item";
 
 import './style.scoped.sass';
 
 function Navbar() {
+    
+    /**
+     * Donwload the cv pdf
+     * 
+     * @return
+     */
+    const downloadCvPdf = () => {
+        alert('descargando pdf');
+    }
+
     return <div className="navbar">
         <div className="navbar-content">
-            <NavbarItem icon="faHome" label="Home" current={true} />
-            <NavbarItem icon="faUser" label="Sobre mi" />
-            <NavbarItem icon="faBriefcase" label="Portafolio" />
-            <NavbarItem icon="faStar" label="Habilidades" />
-            <NavbarItem icon="faGlobe" label="Experiencia" />
-            <NavbarItem icon="faToolbox" label="Eventos" />
-            <NavbarItem icon="faAddressCard" label="Contacto" />
-            <NavbarItem icon="faCloudDownloadAlt" label="CV" />
+            <NavbarItem
+                current={true}
+                icon="faHome"
+                label="Home"
+                to="/"
+            />
+
+            <NavbarItem
+                label="Sobre mi"
+                icon="faUser"
+                to="about-me"
+            />
+
+            <NavbarItem
+                icon="faBriefcase"
+                label="Portafolio"
+                to="portfolio"
+            />
+
+            <NavbarItem
+                label="Habilidades"
+                icon="faStar"
+                to="skills"
+            />
+
+            <NavbarItem
+                label="Experiencia"
+                icon="faGlobe"
+                to="experience"
+            />
+
+            <NavbarItem
+                icon="faToolbox"
+                label="Eventos"
+                to="festivals"
+            />
+
+            <NavbarItem
+                icon="faAddressCard"
+                label="Contacto"
+                to="contact"
+            />
+
+            <NavbarItem
+                icon="faCloudDownloadAlt"
+                label="CV"
+                onClick={downloadCvPdf}
+            />
         </div>
     </div>
 }
