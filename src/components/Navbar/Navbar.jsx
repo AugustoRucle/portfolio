5a002114a73285e9db5d207f2183f9b195fc7e40
Augusto Ruiz Clemente <augusto@realhost.com.mx>
2022-04-08 14:40:10 -0500

    Update style navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 import NavbarItem from "./components/Item";
 
+import {
+    useLocation,
+} from "react-router-dom";
+
 import './style.scoped.sass';
 
 function Navbar() {
-    
+    const location = useLocation();
+
     /**
      * Donwload the cv pdf
      * 
@@ -15,46 +20,62 @@ function Navbar() {
         alert('descargando pdf');
     }
 
+    /**
+     * Check if item is the current pathname
+     * 
+     * @param {string} to
+     * @return {boolean}
+     */
+    const isCurrentPathname = (to) => {
+        return location.pathname === to;
+    }
+
     return <div className="navbar">
         <div className="navbar-content">
             <NavbarItem
-                current={true}
+                current={isCurrentPathname("/")}
                 icon="faHome"
                 label="Home"
                 to="/"
             />
 
             <NavbarItem
+                current={isCurrentPathname("/about-me")}
                 label="Sobre mi"
                 icon="faUser"
                 to="about-me"
             />
 
             <NavbarItem
+                current={isCurrentPathname("/portfolio")}
                 icon="faBriefcase"
                 label="Portafolio"
                 to="portfolio"
             />
 
             <NavbarItem
+                current={isCurrentPathname("/skills")}
                 label="Habilidades"
                 icon="faStar"
                 to="skills"
             />
 
             <NavbarItem
+                current={isCurrentPathname("/experience")}
                 label="Experiencia"
                 icon="faGlobe"
                 to="experience"
             />
 
             <NavbarItem
+                current={isCurrentPathname("/festivals")}
                 icon="faToolbox"
                 label="Eventos"
                 to="festivals"
             />
 
             <NavbarItem
+                current={isCurrentPathname("/contact")}
                 icon="faAddressCard"
                 label="Contacto"
                 to="contact"
