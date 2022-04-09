7b48545cdb3b88a8c8d9009df2e8848499760c57
Augusto Ruiz Clemente <augusto@realhost.com.mx>
2022-04-09 12:19:17 -0500

    Update page home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import PageTransition from '../../components/PageTransition';
 import TypeWriter from '../../components/Typewriter';
-import H1Tag from '../../components/Tags/H1/H1';
-import H3Tag from '../../components/Tags/h3';
+import H1Tag from '../../components/Tags/H1';
+import H3Tag from '../../components/Tags/H3';
 import BrTag from '../../components/Tags/Br';
 import Button from '../../components/Button';
 import Social from '../../components/Social';
+import { Link } from "react-router-dom";
 
 import Avatar from '../../assets/images/Logo.png';
 
@@ -17,33 +18,43 @@ function HomePage() {
         <div className="home-page">
             <div className="left-content">
                 <H3Tag>
-                    <TypeWriter width={800} hideAfter={4200}>
-                        Hola, mi nombre es <BrTag position="left-right"> &nbsp; Augusto Ruiz</BrTag>
-                    </TypeWriter>
+                    Hola, mi nombre es
+
+                    <BrTag parent="h3">
+                        Augusto Ruiz &nbsp;
+                    </BrTag>
+
+                    <div style={{ height: 30 }} />
                 </H3Tag>
 
                 <div style={{ marginTop: 80 }} />
 
                 <H1Tag>
-                    <TypeWriter width={440} startAfter={4200} hideAfter={2000}>
+                    <TypeWriter width={440} startAfter={300} hideAfter={2100}>
                         Soy un
                     </TypeWriter>
 
-                    <div className="mb-3 mt-5" style={{ height: 100 }}>
-                        <BrTag position="top-bottom">
-                            <TypeWriter width={750} startAfter={6100}>
-                                &nbsp; Desarrollador Frontend &nbsp;&nbsp;
-                            </TypeWriter>
-                        </BrTag>
-                    </div>
-                </H1Tag>
+                    <div style={{ height: 10 }} />
 
+                    <BrTag parent="h1">
+                        <TypeWriter width={730} startAfter={2300}>
+                            Desarrollador Frontend &nbsp;
+                        </TypeWriter>
+                    </BrTag>
+
+                    <div style={{ height: 30 }} />
+                </H1Tag>
 
                 <div style={{ marginTop: 65 }} />
 
                 <div className="about-me">
                     <div className="about-me--animation">
-                        <Button label="Conoce mis trabajos" icon="faArrowRight" />
+                        <Link to="about-me">
+                            <Button
+                                label="Conoce mis trabajos"
+                                icon="faArrowRight"
+                            />
+                        </Link>
                     </div>
                 </div>
             </div>
