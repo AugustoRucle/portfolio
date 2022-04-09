f89e854ac5a1681d7d4a7ad52f97b144102e8574
Augusto Ruiz Clemente <augusto@realhost.com.mx>
2022-04-09 12:12:55 -0500

    new functions

--- src/utils/ functions.js
+++ src/utils/ functions.js
@@ -8,6 +8,75 @@ const isFunction = (value) => {
     return value && typeof value === 'function';
 }
 
+/**
+ * Get slice a array in chunks
+ * 
+ * @param {array} array 
+ * @param {number} chunkSize 
+ * @returns 
+ */
+const sliceIntoChunks = (array, chunkSize) => {
+    const chunks = [];
+
+    if (chunkSize === 0) {
+        return [array];
+    }
+
+    for (let i = 0; i < array.length; i += chunkSize) {
+        const chunk = array.slice(i, i + chunkSize);
+        chunks.push(chunk);
+    }
+
+    return chunks;
+}
+
+/**
+ * Generate componente key
+ * 
+ * @param {string} key
+ * @return {string}
+ */
+const generateComponentKey = (key) => {
+    return `${generateRandomId()}${key ? '__' + key : ''}`;
+}
+
+/**
+ * Generate random id.
+ * 
+ * @return {string}
+ */
+const generateRandomId = () => {
+    return `${generateRandomAlphanumeric()}_${generateRandomAlphanumeric()}_${Date.now()}`;
+}
+
+/**
+ * Generate random alphanumeric.
+ * 
+ * @return {string}
+ */
+const generateRandomAlphanumeric = (size = 5) => {
+    let zeros = size > 1
+        ? "0".repeat(size - 1)
+        : "";
+
+    return Math.floor((1 + Math.random()) * `0x1${zeros}`).toString(16);
+}
+
+/**
+ * Generate a random number
+ * 
+ * @param {number} min 
+ * @param {number} max 
+ * @returns {number}
+ */
+const getRandomArbitrary = (min, max) => {
+    return Math.abs(Math.ceil(Math.random() * (max - min) + min));
+}
+
 export {
+    generateComponentKey,
+    getRandomArbitrary,
+    generateRandomId,
+    sliceIntoChunks,
     isFunction,
 }
