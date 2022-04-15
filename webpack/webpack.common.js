509cd8a169339c3d1faad4e7fbdd7e2fd78686ce
Augusto Ruiz Clemente <augusto@realhost.com.mx>
2022-04-14 21:28:17 -0500

    Update webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -25,7 +25,7 @@ module.exports = {
                 use: ['babel-loader'],
             },
             {
-                test: /\.(png|jpg|gif)$/i,
+                test: /\.(png|jpg|webp)$/i,
                 use: [
                     {
                         loader: 'url-loader',
